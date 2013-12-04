a5ff819164026af2d8bc9297c870217d40e66f86
Sam Walton <sam.walton@givegw.org>
2013-12-04 13:00:23 -0600

    move rb all

---
+++ my-mov.rb
@@ -0,0 +1,30 @@
+require 'tempfile'
+
+LINE_MAX = 3000
+temp_file = Tempfile.new('foo')
+line_counter = 0
+begin
+  File.open("H:/Diggi Reports/data.txt", 'r') do |file|
+    file.each_line do |line|
+      if line.match /Guard/
+	    temp_file.write line
+		line_counter += 1
+	  end
+    end
+  end
+  temp_file.rewind
+FileUtils.cp(temp_file.path, "H:/Diggi Reports/flash.txt")
+#File.open("H:/Diggi Reports/flash.txt", "w") do |out_file|
+ # File.foreach(temp_file).each_with_index { |my_line, i|
+  #  out_file.puts my_line unless i < LINE_MAX }
+	#end
+#end
+ensure
+  temp_file.close
+  temp_file.unlink
+end 
+line_counter
+
+
+
+#FileUtils.mv(output_file, input_file)
