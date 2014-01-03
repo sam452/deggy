8a23a0d7f44f1a6001ae47664c65e466e521676a
Sam Walton <sam@foofrancine.com>
2014-01-03 15:48:14 -0600

    Get schtasks set up

---
+++ onlineGW.rb
@@ -0,0 +1,12 @@
+require 'open-uri'
+require 'nokogiri'
+
+target = "http://www.shopgoodwill.com/search/SearchKey.asp?itemTitle=&catid=0&sellerID=117&closed=no&minPrice=&maxPrice=&sortBy=itemEndTime&SortOrder=a&showthumbs=on"
+doc = Nokogiri::HTML(open(target))
+
+my_text = doc.css('div.mainbluebox h1.whitetext').text
+
+my_numbers = my_text.gsub(/,(?=\d{3}\b)/, '')
+my_number = my_numbers.scan(/\d+/)[0]
+my_number = my_numbers.scan(/\d+/)[0]
+print my_number
