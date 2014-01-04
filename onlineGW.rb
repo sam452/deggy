8f5c7e3d72ba44618c709dc461decc6ff66e6568
sam walton <sam@foofrancine.com>
2014-01-03 18:49:15 -0600

    set for unix

--- onlineGW.rb
+++ onlineGW.rb
@@ -1,6 +1,9 @@
 require 'open-uri'
 require 'nokogiri'
 
+t = Time.now
+current_time = t.strftime("%c")
+
 target = "http://www.shopgoodwill.com/search/SearchKey.asp?itemTitle=&catid=0&sellerID=117&closed=no&minPrice=&maxPrice=&sortBy=itemEndTime&SortOrder=a&showthumbs=on"
 doc = Nokogiri::HTML(open(target))
 
@@ -9,4 +12,8 @@ my_text = doc.css('div.mainbluebox h1.whitetext').text
 my_numbers = my_text.gsub(/,(?=\d{3}\b)/, '')
 my_number = my_numbers.scan(/\d+/)[0]
 my_number = my_numbers.scan(/\d+/)[0]
-print my_number
+print  "#{current_time + " " + my_number}"
+
+open('myfile.txt', 'a') { |f|
+  f.puts "#{current_time + " " + my_number}"
+}
