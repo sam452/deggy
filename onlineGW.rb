083ded291183c6c093c521e929b15328a155b333
sam walton <sam@foofrancine.com>
2014-01-05 19:56:55 -0600

    put time closer to end

--- onlineGW.rb
+++ onlineGW.rb
@@ -1,8 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 
-t = Time.now
-current_time = t.strftime("%c")
+
 
 target = "http://www.shopgoodwill.com/search/SearchKey.asp?itemTitle=&catid=0&sellerID=117&closed=no&minPrice=&maxPrice=&sortBy=itemEndTime&SortOrder=a&showthumbs=on"
 doc = Nokogiri::HTML(open(target))
@@ -12,6 +11,8 @@ my_text = doc.css('div.mainbluebox h1.whitetext').text
 my_numbers = my_text.gsub(/,(?=\d{3}\b)/, '')
 my_number = my_numbers.scan(/\d+/)[0]
 my_number = my_numbers.scan(/\d+/)[0]
+t = Time.now
+current_time = t.strftime("%c")
 print  "#{current_time + " " + my_number}"
 
 open('myfile.txt', 'a') { |f|
